196d53a63c109f051841193ef9cdb763faad739a
brokenhalo420 <m.nikolov1501@gmail.com>
2023-07-04 02:59:32 +0300

    redis

--- flickr-server/src/auth/auth-server.ts
+++ flickr-server/src/auth/auth-server.ts
@@ -2,10 +2,17 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import * as jwt from "jsonwebtoken";
 import { User } from "../models/user";
+import { createClient } from "redis";
 dotenv.config();
+
 
 const app: Express = express();
 const port = process.env.AUTH_PORT;
+
+const redisClient = createClient({
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    password: process.env.REDIS_PASS
+});
 
 app.use(express.json());
 
@@ -14,14 +21,18 @@ let refreshTokens: string[] = [];
 app.post('/token', (req, res) => {
     const refreshToken: string = req.body.token as string;
     if (refreshToken == null) return res.sendStatus(401);
-    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
 
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET as string, (err, user) => {
         if (err) return res.sendStatus(403);
         let tempUser: User = (user as User);
         let resUser: User = new User(tempUser.username, tempUser.password);
-        const accessToken: string = generateAccessToken(resUser)
-        res.json({ accessToken: accessToken })
+        redisClient.get(resUser.username).then(token => {
+            const accessToken: string = generateAccessToken(resUser)
+            res.json({ accessToken: accessToken })
+        }).catch(err => {
+            console.log(err);
+            return res.sendStatus(403);
+        });
     })
 })
 
@@ -37,7 +48,7 @@ app.post("/login", (req: Request, res: Response) => {
     const user: User = new User(username, password);
     const accessToken = generateAccessToken(user);
     const refreshToken = jwt.sign({ user }, process.env.REFRESH_TOKEN_SECRET as string);
-    refreshTokens.push(refreshToken);
+    redisClient.setEx(username, 2 * 60 * 60, refreshToken);
     res.json({
         accessToken: accessToken,
         refreshToken: refreshToken
---
+++ flickr-server/src/cache/cache-helpers.ts
@@ -0,0 +1,13 @@
+// import dotenv from "dotenv";
+// import {createClient } from "redis";
+// dotenv.config();
+
+// const redisClient = createClient({
+//   url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+//   password: process.env.REDIS_PASS
+// });
+
+// redisClient.on('connect', () => {
+//   console.log('redis client connected');
+// })
+// export default redisClient;
