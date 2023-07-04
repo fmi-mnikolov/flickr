f3e2e69ca4f231acda5e1d128e7ed3e88e9e8ca4
brokenhalo420 <m.nikolov1501@gmail.com>
2023-07-04 04:28:45 +0300

    using redis now

--- flickr-server/src/auth/auth-server.ts
+++ flickr-server/src/auth/auth-server.ts
@@ -1,44 +1,55 @@
+import dotenv from 'dotenv';
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
 import * as jwt from "jsonwebtoken";
 import { User } from "../models/user";
-import { createClient } from "redis";
+import * as redis from "redis";
 dotenv.config();
 
 
 const app: Express = express();
 const port = process.env.AUTH_PORT;
 
-const redisClient = createClient({
-    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-    password: process.env.REDIS_PASS
+const redisClient = redis.createClient({
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
 });
+redisClient.connect();
+
+
 
 app.use(express.json());
 
-let refreshTokens: string[] = [];
-
 app.post('/token', (req, res) => {
-    const refreshToken: string = req.body.token as string;
-    if (refreshToken == null) return res.sendStatus(401);
+    let token = req.body.token;
+    if (token == null) return res.sendStatus(401);
 
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET as string, (err, user) => {
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET as string, (err, user) => {
         if (err) return res.sendStatus(403);
-        let tempUser: User = (user as User);
-        let resUser: User = new User(tempUser.username, tempUser.password);
-        redisClient.get(resUser.username).then(token => {
+        let tempUser: { user: User } = user;
+        let resUser: User = new User(tempUser.user.username, tempUser.user.password);
+        redisClient.GET(resUser.username).then(dbtoken => {
+            if (token !== dbtoken) return res.sendStatus(403);
             const accessToken: string = generateAccessToken(resUser)
             res.json({ accessToken: accessToken })
         }).catch(err => {
-            console.log(err);
             return res.sendStatus(403);
-        });
+        })
     })
 })
 
 app.delete('/logout', (req, res) => {
-    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-    res.sendStatus(204)
+    let token = req.body.token;
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET as string, (err, user) => {
+        if (err) return res.sendStatus(403);
+        let tempUser: { user: User } = user;
+        let resUser: User = new User(tempUser.user.username, tempUser.user.password);
+        redisClient.GET(resUser.username).then(dbtoken => {
+            if (token !== dbtoken) return res.sendStatus(403);
+            redisClient.DEL(resUser.username);
+            return res.sendStatus(204);
+        })
+    })
 })
 
 app.post("/login", (req: Request, res: Response) => {
@@ -47,7 +58,7 @@ app.post("/login", (req: Request, res: Response) => {
 
     const user: User = new User(username, password);
     const accessToken = generateAccessToken(user);
-    const refreshToken = jwt.sign({ user }, process.env.REFRESH_TOKEN_SECRET as string);
+    let refreshToken = jwt.sign({ user }, process.env.REFRESH_TOKEN_SECRET as string);
     redisClient.setEx(username, 2 * 60 * 60, refreshToken);
     res.json({
         accessToken: accessToken,
